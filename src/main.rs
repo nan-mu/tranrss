fb4261f9dcfb869e037ba8db010aa4cba675980e
nan mu <mu.nan.11@outlook.com>
2024-02-20 18:06:36 +0800

    通过日志发现了之前异步的问题，现在是真并发

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use bytes;
 use feed_rs::parser;
-use log::info;
+use log::{debug, info};
 use log4rs;
 use reqwest::get;
 use std::error::Error;
+use tokio::spawn;
 
 mod test;
 
@@ -13,20 +14,32 @@ pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 async fn main() -> Result<()> {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
     info!("start");
-    let urls = vec!["https://rustcc.cn/rss", "https://rustmagazine.org/feed.xml"];
+    let urls = vec![
+        "https://rustcc.cn/rss",             //RSS 2.O
+        "https://rustmagazine.org/feed.xml", //Atom
+        "https://readrust.net/all/feed.rss", //RSS 1.O
+    ];
     let texts = get_text(urls);
     for content in texts.await? {
         let feed = parser::parse(&content[..])?;
-        println!("{:?}", feed.title);
+        info!("{:?}", feed.feed_type);
         // info!("{:?}", feed);
     }
     Ok(())
 }
 
-async fn get_text(urls: Vec<&str>) -> Result<Vec<bytes::Bytes>> {
+async fn get_text(urls: Vec<&'static str>) -> Result<Vec<bytes::Bytes>> {
     let mut res = Vec::new();
+    let mut connects = Vec::new();
     for url in urls {
-        res.push(get(url).await?.bytes().await?);
+        debug!("访问 {} 开始", url);
+        connects.push(spawn(get(url)));
+        // res.push(get(url).await?.bytes().await?);
     }
+    for connect in connects {
+        debug!("访问结束");
+        res.push(connect.await??.bytes().await?);
+    }
+
     Ok(res)
 }
