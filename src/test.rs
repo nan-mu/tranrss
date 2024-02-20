2dc89ea32781e329a98804f53e446ed12b712eb5
nan mu <mu.nan.11@outlook.com>
2024-02-20 15:46:10 +0800

    很奇怪，写一会rust-analyzer就要挂，网上查找资料无果，准备换个地方写

--- src/test.rs
+++ src/test.rs
@@ -4,7 +4,7 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 async fn test_async_get_rss() -> Result<()> {
     use crate::get_text;
     use rss::Channel;
-
+    //这里的第二个链接其实不算rss（可能是叫Atom），需要跟换库
     let urls = vec!["https://rustcc.cn/rss", "https://rustmagazine.org/feed.xml"];
     let texts = get_text(urls).await?;
     for content in texts {
