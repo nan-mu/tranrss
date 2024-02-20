cbdd325f44f28f42f5cd8288f7adb088e6b98524
nan mu <mu.nan.11@outlook.com>
2024-02-20 17:16:10 +0800

    添加了日志

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bytes;
 use feed_rs::parser;
+use log::info;
+use log4rs;
 use reqwest::get;
 use std::error::Error;
 
@@ -9,13 +11,14 @@ pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    //https://rustcc.cn/rss
-    //https://rustmagazine.org/feed.xml
+    log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+    info!("start");
     let urls = vec!["https://rustcc.cn/rss", "https://rustmagazine.org/feed.xml"];
     let texts = get_text(urls);
     for content in texts.await? {
         let feed = parser::parse(&content[..])?;
         println!("{:?}", feed.title);
+        // info!("{:?}", feed);
     }
     Ok(())
 }
