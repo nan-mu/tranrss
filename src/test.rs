9fa1b8d021a63c11a78a66112005443019b9515b
nan mu <mu.nan.11@outlook.com>
2024-02-22 17:41:00 +0800

    补充了readme 补充了测试函数的声明

--- src/test.rs
+++ src/test.rs
@@ -2,7 +2,8 @@
 use crate::Result as _Result;
 
 #[tokio::test]
-async fn test() -> _Result<()> {
+async fn test_get_rss() -> _Result<()> {
+    //验证网络库是否可用
     use crate::get_text;
     use feed_rs::parser;
     let urls = vec!["https://rustcc.cn/rss", "https://rustmagazine.org/feed.xml"];
